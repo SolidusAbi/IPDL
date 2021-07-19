d103b2da2141fef384d83486fa3e39fbd210db06
Abián Hernández <abian.hernandez@ulpgc.es>
2021-07-19 17:02:44 +0100

    Visualization utils updated

--- IPDL/utils.py
+++ IPDL/utils.py
@@ -27,8 +27,8 @@ def showMutualInformation(ip: InformationPlane, moving_average_n = 15, colors: l
         gs1 = fig.add_gridspec(nrows=1, ncols=1, left=0.05, right=0.84, wspace=0.05)
         gs2 = fig.add_gridspec(nrows=1, ncols=len(Ixts), left=0.85, right=0.95, wspace=0)
         f8_ax1 = fig.add_subplot(gs1[:, :])
-        f8_ax1.set_xlabel("I(X, T)")
-        f8_ax1.set_ylabel("I(T, Y)")
+        f8_ax1.set_xlabel("I($X$, $T$)")
+        f8_ax1.set_ylabel("I($T$, $Y$)")
 
         for idx, Ixt in enumerate(Ixts):
             Ity = Itys[idx]
@@ -66,11 +66,11 @@ def show_ip(ip_df: pd.DataFrame, n=25, labels=[], moving_average_n=25):
 
     with plt.style.context('seaborn'):
         fig = plt.figure(constrained_layout=False)
-        gs1 = fig.add_gridspec(nrows=1, ncols=1, left=0.05, right=0.84, wspace=0.05)
+        gs1 = fig.add_gridspec(nrows=1, ncols=1, left=0.08, right=0.84, wspace=0.05)
         gs2 = fig.add_gridspec(nrows=1, ncols=len(ip_df.keys()[::2]), left=0.85, right=0.95, wspace=0)
         f8_ax1 = fig.add_subplot(gs1[:, :])
-        f8_ax1.set_xlabel("I(X, T)")
-        f8_ax1.set_ylabel("I(T, Y)")
+        f8_ax1.set_xlabel("I($X$, $T$)", fontsize=14)
+        f8_ax1.set_ylabel("I($T$, $Y$)", fontsize=14)
 
         for idx, (layer, _) in enumerate(ip_df.columns[::2]):
             Ity = moving_average(ip_df[layer]['Ity'].to_numpy(), n=moving_average_n, padding_size=int(moving_average_n*0.2))
@@ -79,7 +79,7 @@ def show_ip(ip_df: pd.DataFrame, n=25, labels=[], moving_average_n=25):
             cmap = plt.cm.get_cmap(colors[idx])
             iterations = np.geomspace(1, len(Ity)-1, num=n, dtype=np.uint)
 
-            color = np.array([cmap(iterations[int(len(iterations)*0.65)])])
+            color = np.array([cmap(iterations[int(len(iterations)*0.75)])])
             sc = f8_ax1.scatter(Ixt[iterations], Ity[iterations], c=iterations, vmin=0, vmax=iterations.max(), cmap=cmap, edgecolor=color)
             if not labels:
                 f8_ax1.scatter([], [], c=color, label=layer)
@@ -90,10 +90,9 @@ def show_ip(ip_df: pd.DataFrame, n=25, labels=[], moving_average_n=25):
             cb = fig.colorbar(sc, cax=f8_ax2, pad=0)
             cb.set_ticks([])
 
-
-
         f8_ax1.legend()
         cb.set_ticks([0, iterations.max()])
+        f8_ax2.set_yticklabels(['0', ip_df[layer]['Ixt'].size])
         cb.set_label("Iterations", labelpad=-18)
 
     return fig
@@ -115,8 +114,8 @@ def show_aeip(ip_df: pd.DataFrame, ref_entropy: float, n=25, moving_average_n=25
     encoder_idx = np.arange(1, btnck_idx+1)
     decoder_idx = np.arange(btnck_idx-1, 0, step=-1)
 
-    labels_encoder = list(map(lambda x: 'Encoder {}'.format(x) if x != btnck_idx else 'Bottleneck', encoder_idx))
-    labels_decoder = list(map(lambda x: 'Decoder {}'.format(x) if x != btnck_idx else 'Bottleneck', decoder_idx))
+    labels_encoder = list(map(lambda x: '$E_%d$' % x if x != btnck_idx else '$Z$', encoder_idx))
+    labels_decoder = list(map(lambda x: '$D_%d$' % x if x != btnck_idx else '$Z$', decoder_idx))
     labels = labels_encoder + labels_decoder
     markers = markers[:btnck_idx] + markers[:btnck_idx-1][::-1]
 
@@ -127,15 +126,15 @@ def show_aeip(ip_df: pd.DataFrame, ref_entropy: float, n=25, moving_average_n=25
         # Encoder axis
         f8_ax1 = fig.add_subplot(gs1[0:9, 0])
         f8_ax1.set_title("Encoder", fontsize=18)
-        f8_ax1.set_xlabel("I(X, T)", fontsize=14)
-        f8_ax1.set_ylabel("I(T, Y)", fontsize=14)
+        f8_ax1.set_xlabel("I($X$, $T$)", fontsize=14)
+        f8_ax1.set_ylabel("I($T$, $Y$)", fontsize=14)
         f8_ax1.plot([0, 1], [0, 1], transform=f8_ax1.transAxes, linestyle='dashed')
 
         # Decoder axis
         f8_ax2 = fig.add_subplot(gs1[0:9, 1])
         f8_ax2.set_title("Decoder", fontsize=18) 
-        f8_ax2.set_xlabel("I(X, T)", fontsize=14)
-        f8_ax2.set_ylabel("I(T, Y)", fontsize=14, rotation=270, labelpad=20)
+        f8_ax2.set_xlabel("I($X$, $T$)", fontsize=14)
+        f8_ax2.set_ylabel("I($T$, $Y$)", fontsize=14, rotation=270, labelpad=20)
         f8_ax2.yaxis.tick_right()
         f8_ax2.yaxis.set_label_position("right")
         f8_ax2.plot([0, 1], [0, 1], transform=f8_ax2.transAxes, linestyle='dashed')
@@ -203,3 +202,4 @@ def gen_log_space(limit: int, n: int) -> np.ndarray:
     
     # round, re-adjust to 0 indexing (i.e. minus 1) and return np.uint64 array
     return np.array(list(map(lambda x: round(x)-1, result)), dtype=np.uint64)
+    
